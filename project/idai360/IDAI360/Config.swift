ff7fdd27b78964b0f3f0177a8c6bccb73cae9951
q334530870 <yaojie1122@126.com>
2016-02-26 17:18:32 +0800

    2

--- project/idai360/IDAI360/Config.swift
+++ project/idai360/IDAI360/Config.swift
@@ -3,5 +3,5 @@ let MAIN_COLOR = UIColor(red: 46/255, green: 167/255, blue: 224/255, alpha: 1.0)
 //网站地址
 let MAIN_URL = "http://demo.idai360.com"
 //API接口地址
-//let API_URL = "http://api.idai360.com"
-let API_URL = "http://192.168.0.104:81"
+let API_URL = "http://api.idai360.com"
+//let API_URL = "http://192.168.0.104:81"
