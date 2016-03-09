0d11391e14487a4d9fccf454a9ec6a9e4e64a6dc
q334530870 <yaojie1122@126.com>
2016-03-09 13:11:33 +0800

    11

--- source/xunzhaodoubiyu/xunzhaodoubiyu/ViewController.swift
+++ source/xunzhaodoubiyu/xunzhaodoubiyu/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     var imgs = [UIImageView]()
     var buttons = [UIButton]()
     var rst = [Int]()
@@ -22,24 +22,24 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        var background = UIImageView(frame: CGRectMake(0, 0, self.view.bounds.width, self.view.bounds.height))
+        let background = UIImageView(frame: CGRectMake(0, 0, self.view.bounds.width, self.view.bounds.height))
         background.image = UIImage(named: "sea.jpg")
         self.view.addSubview(background)
         
-        var width = 190
-        var twoWidth = 95
-        var d0 = UIImage(named: "d6.png")
-        var d1 = UIImage(named: "d1.png")
-        var d2 = UIImage(named: "d2.png")
-        var d3 = UIImage(named: "d3.png")
-        var d4 = UIImage(named: "d4.png")
-        var d5 = UIImage(named: "d5.png")
-        var d6 = UIImage(named: "d6.png")
+        let width = 190
+        let twoWidth = 95
+        let d0 = UIImage(named: "d6.png")
+        let d1 = UIImage(named: "d1.png")
+        let d2 = UIImage(named: "d2.png")
+        let d3 = UIImage(named: "d3.png")
+        let d4 = UIImage(named: "d4.png")
+        let d5 = UIImage(named: "d5.png")
+        let d6 = UIImage(named: "d6.png")
         animateImg = [d1!,d2!,d3!,d4!,d5!,d6!]
         
         for var i = 0; i < 5; i++ {
-            var img = UIImageView(frame: CGRectMake((CGFloat)(i*width + 4), 20, 180, 105))
-            var btn = UIButton(frame: CGRectMake((CGFloat)(i*width + 4), 20, 180, 105))
+            let img = UIImageView(frame: CGRectMake((CGFloat)(i*width + 4), 20, 180, 105))
+            let btn = UIButton(frame: CGRectMake((CGFloat)(i*width + 4), 20, 180, 105))
             btn.addTarget(self, action: "clickImg:", forControlEvents: UIControlEvents.TouchUpInside)
             btn.tag = i+1
             self.view.addSubview(btn)
@@ -55,20 +55,20 @@ class ViewController: UIViewController {
     }
     
     func clickImg(btn:UIButton){
-        var tag = btn.tag
+        let tag = btn.tag
         if tag != 0 && !win{
             currentTag = tag
             var random = (Int)(arc4random() % 5)
             while indexOfArray(rst,obj: random){
                 random = (Int)(arc4random() % 5)
             }
-            var name = "f\(random+1).png"
+            let name = "f\(random+1).png"
             if name == winName {
                 win = true
             }
             randomImg = UIImage(named: name)!
-            var temp = animateImg
-            imgs[tag-1].animationImages = temp
+            let temp = animateImg
+            imgs[tag-1].animationImages = temp as? [UIImage]
             imgs[tag-1].animationDuration = 1
             imgs[tag-1].animationRepeatCount = 1
             imgs[tag-1].startAnimating()
@@ -79,12 +79,12 @@ class ViewController: UIViewController {
     }
     
     func stopImg(){
-       imgs[currentTag-1].image = randomImg
+        imgs[currentTag-1].image = randomImg
         if win{
             showAlert(self, title: "恭喜你！", message: "成功找到了逗比鱼。。。")
         }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
@@ -99,13 +99,13 @@ class ViewController: UIViewController {
         }
         return bool
     }
-
+    
     func showAlert(view:UIViewController,title:String,message:String){
-        var alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.Alert)
-        var ok = UIAlertAction(title: "确定", style: UIAlertActionStyle.Default, handler: nil)
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.Alert)
+        let ok = UIAlertAction(title: "确定", style: UIAlertActionStyle.Default, handler: nil)
         alert.addAction(ok)
         view.presentViewController(alert, animated: true, completion: nil)
     }
-
+    
 }
 
